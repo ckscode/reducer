e870aa3db8f5d57ed7fa00f7809fd3f9bfcf4f40
ckscode <cksiddhaarthan@gmail.com>
2024-05-13 14:00:26 +0530

    new changes

--- src/Components/UseReducer/UseReducer.jsx
+++ src/Components/UseReducer/UseReducer.jsx
@@ -2,13 +2,21 @@ import React, { useReducer } from 'react';
 import ReducerAction from './ReducerAction';
 
 const UseReducer = () => {
-    const [state,dispatch] = useReducer(ReducerAction,0)
+    const [state,dispatch] = useReducer(ReducerAction,0);
+    const handleSub = () =>{
+           
+             if(state>0 && state !== 1){
+                dispatch({type:'SUB',payload:2})
+             }else if(state===1){
+                dispatch({type:'SUB',payload:1})
+             }
+    }
     return (
         <div>
             <h1>USeReducer Hook</h1>
             <p>{state}</p>
             <button onClick={()=>dispatch({type:'ADD',payload:5})}>add</button>
-            <button onClick={()=>state>0?dispatch({type:'SUB',payload:2}):dispatch({type:'SUB',payload:0})}>sub</button>
+            <button onClick={handleSub}>sub</button>
             <button onClick={()=>dispatch({type:'RESET',payload:0})}>Reset</button>
         </div>
     );
